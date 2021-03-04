a5c42df2ab5eea6e57f62958a3164e85c83f2a50
Rakha21 <74606788+Rakha21@users.noreply.github.com>
2021-03-04 11:15:27 +0700

    Add files via upload

---
+++ DAHLAH/lib/donasi.js
@@ -0,0 +1,7 @@
+const donasi = () => { 
+	return `
+┣➥ *PULSA* : 0821-3874-6029
+┣➥ *GOPAY* : 0821-3874-6029`
+}
+
+exports.donasi = donasi
---
+++ DAHLAH/lib/help.js
@@ -0,0 +1,73 @@
+
+const help = (prefix) => { 
+	return `
+Prefix: 「  ${prefix} 」
+Creator: DAHLAH ESCE JELEK
+Owner: DAHLAH ESCE JELEK
+
+${prefix}help
+${prefix}bc
+${prefix}speed
+${prefix}setprefix
+${prefix}fordward
+${prefix}setstatus
+${prefix}fordward1
+${prefix}readall
+${prefix}runtime
+${prefix}hilih
+${prefix}holoh
+${prefix}ssphone
+${prefix}sspc
+${prefix}pussy
+${prefix}moddroid
+${prefix}tabok
+${prefix}toptt
+${prefix}bass
+${prefix}terbalik
+${prefix}huruf
+${prefix}carbon
+${prefix}kiss
+${prefix}nsfwgif
+${prefix}randomquran
+${prefix}fml
+${prefix}other
+${prefix}yt2mp3
+${prefix}play
+${prefix}joox
+${prefix}asci
+${prefix}clone
+${prefix}ytsearch
+${prefix}gas
+${prefix}halo
+${prefix}xl
+${prefix}film
+${prefix}qrcode
+${prefix}toimg
+${prefix}sticker
+${prefix}gtts
+${prefix}meme
+${prefix}memeindo
+${prefix}puisi
+${prefix}memeind
+${prefix}darkjoke
+${prefix}pinsearch
+${prefix}harta
+${prefix}lovetext
+${prefix}group buka|tutup
+${prefix}promote
+${prefix}demote
+${prefix}add
+${prefix}kick
+${prefix}welcome 0|1
+${prefix}listadmin
+${prefix}tagall
+${prefix}cgc
+${prefix}tagall2
+${prefix}tagall3
+${prefix}ownergc
+${prefix}leave
+${prefix}setpp`
+}
+exports.help = help
+
+  
